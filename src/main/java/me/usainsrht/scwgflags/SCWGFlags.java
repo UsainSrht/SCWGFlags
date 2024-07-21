1e3182ea0cd7841c4f48583382fdb33bd58e84d6
UsainSrht <usainsrht@gmail.com>
2024-07-21 20:02:30 +0300

    Initial commit

---
+++ src/main/java/me/usainsrht/scwgflags/SCWGFlags.java
@@ -0,0 +1,17 @@
+package me.usainsrht.scwgflags;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SCWGFlags extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
